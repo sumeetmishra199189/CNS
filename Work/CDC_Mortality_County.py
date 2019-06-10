5d85ffaf9f19277f2e24a40c356d87088552d31d
sumeetmishra199189 <sumeetmishra1991@gmail.com>
2019-06-10 11:08:37 -0400

    Initial commit

---
+++ Work/CDC_Mortality_County.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  6 12:16:24 2019
+
+@author: sumeetmishra
+"""
+
+# Import Dependencies
+#%matplotlib inline
+
+# Start Python Imports
+import math, time, random, datetime
+
+# Data Manipulation
+import numpy as np
+import pandas as pd
+
+# Visualization 
+import matplotlib.pyplot as plt
+import missingno
+import seaborn as sns
+plt.style.use('seaborn-whitegrid')
+
+# Preprocessing
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder, label_binarize
+
+# Machine learning
+import catboost
+from sklearn.model_selection import train_test_split
+from sklearn import model_selection, tree, preprocessing, metrics, linear_model
+from sklearn.svm import LinearSVC
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LinearRegression, LogisticRegression, SGDClassifier
+from sklearn.tree import DecisionTreeClassifier
+from catboost import CatBoostClassifier, Pool, cv
+
+# Let's be rebels and ignore warnings for now
+#import warnings
+#warnings.filterwarnings('ignore')
+
+
+
+mortality_c=pd.read_csv('/Users/sumeetmishra/Desktop/CNS/CDCMortality/Data/NCHS_-_Drug_Poisoning_Mortality_by_County__United_States.csv')
+
+mortality_c.head(15)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
